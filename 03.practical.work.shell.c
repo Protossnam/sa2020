aad3349bfb84837b0a9ac91223e460948435e0a9
Protossnam <protossnamjune2nd@gmail.com>
2020-03-13 13:40:35 +0700

    Refined Practical Work 03

--- 03.practical.work.shell.c
+++ 03.practical.work.shell.c
@@ -1,32 +1,48 @@
-#include<string.h>
 #include<sys/types.h>
-#include<sys/wait.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
 
+void parseArg(char *cmd, char **argv)
+{
+	while (*cmd != '\0') 
+	{       
+		while (*cmd == ' ' || *cmd == '\t' || *cmd == '\n') *cmd++ = '\0';
+		*argv++ = cmd;
+		while (*cmd != '\0' && *cmd != ' ' && *cmd != '\t' && *cmd != '\n') cmd++;
+	}
+	*argv = '\0';
+}
+
 int main()
 {
 	char cmd[255];
-	
-	while (1)
-	{
+	char *argv[16];
+
+	while (1) 
+	{                   
 		printf("Command: ");
-		scanf("%s", cmd);
+		gets(cmd);
+		parseArg(cmd, argv);
+		if (strcmp(argv[0], "quit") == 0) exit(0);
+		
+		int pid;
+		int done;
 
-		if (cmd == "quit") return 0;
-
-		int pid = fork();
-		if (pid == 0)
+		if ((pid = fork()) < 0) exit(1);
+		else if (pid == 0) 
 		{
-			char *args[] = {cmd, NULL};
-           		execvp(cmd, args);
-        	}
-		else 
-		{
-			wait(NULL);
-			printf("\n");
+			if (execvp(*argv, argv) < 0) 
+			{
+				printf("Undefined command. Please try again... \n");
+				exit(1);			
+			}
 		}
-    	}	
-	return 0;
+		else 
+		{                                  
+			while (wait(&done) != pid)       
+			;
+		}
+		printf("\n");
+	}
 }
